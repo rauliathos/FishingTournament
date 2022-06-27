3cc985e607949b3416c3ff0211c652009c618b75
rauliathos <rauliathos@gmail.comgit config --global user.email rauliathos@gmail.comgit config --global user.email rauliathos@gmail.com>
2022-06-27 13:06:50 +0000

    changed class model name from Anglerz to teams

--- application/routes.py
+++ application/routes.py
@@ -1,37 +1,50 @@
 from application import app, db
-from application.models import Angler, Catches
-from application.forms import AnglerForm
+from application.models import Teams, Catches
+from application.forms import TeamsForm
 from flask import redirect, request, url_for, render_template
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+#@app.before_first_request
+#def create_tables():
+ #   db.create_all()
 
 @app.route('/')
 def index():
-    angler = Angler.query.all()
-    catches = Catches.query.all()
-    return render_template('layout.html', angler=angler, catches=catches)   
-@app.route('/home')
-def home():
-    return render_template('home.html') 
+    teams = Teams.query.all()
+    print(teams)
+    return render_template("team.html",teams=team)   
+
 
 @app.route('/about')
 def about():
     return render_template("about.html")
 
-@app.route('/add_angler', methods=['GET','POST'])
-def add_angler():
-    form = AnglerForm()
+@app.route('/add_team', methods=['GET','POST'])
+def team():
+    form = TeamsForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            anglerData = Angler(
+            teamData = Teams(
                 team = form.team.data,
                 email = form.email.data,
                 fee = form.fee.data
             )
-            db.session.add(anglerData)
+            db.session.add(teamData)
             db.session.commit()
-            return redirect(url_for('index'))
-    return render_template('addangler.html', form=form)
+            print(teamData)
+            return redirect(url_for('team'))
+    return render_template('add_team.html', form=form)
 
+
+@app.route('/fee_paid/<int:id>')
+def fee_paid(id):
+    team = Teams.query.get(id)
+    team.fee = True
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/fee_not_paid/<int:id>')
+def fee_not_paid(id):
+    team = Teams.query.get(id)
+    team.fee = False
+    db.session.commit()
+    return redirect(url_for('index'))
