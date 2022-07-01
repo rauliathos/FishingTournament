3aeb79036d97505c121015877d49b4c1669ab251
rauliathos <rauliathos@gmail.comgit config --global user.email rauliathos@gmail.comgit config --global user.email rauliathos@gmail.com>
2022-07-01 02:02:08 +0000

    tests 100% coverage

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+from modulefinder import Module
 from flask import url_for
 from flask_testing import TestCase
 from application import app, db
@@ -24,20 +25,18 @@ class TestBase(TestCase):
         #db.drop_all()
         db.create_all()
         # Create test registree
-        teamSample = Teams(team="Sample1", email="sample1@sample.com", fee= True)
-        db.session.add(teamSample)
+        test_team = Teams(team="Sample1", email="sample1@sample.com", fee= True)
+        db.session.add(test_team)
         db.session.commit()
         
-        teamSample =  Teams(team="Sample2", email="sample2@sample.com", fee= False)
-        db.session.add(teamSample)
+        test_team =  Teams(team="Sample2", email="sample2@sample.com", fee= False)
+        db.session.add(test_team)
         db.session.commit()
         
         
-        fishes =["Cod", "Red Cod","Whiting","Mackerel",
-            "Dab", "Coalfish", "Plaice","Ling",
-            "Bass", "Flounder", "Eel", "Pollack"]
-        catchSample= Catches(team ="Sample1", species=fishes, weight = 1.23)
-        db.session.add(catchSample)
+        
+        test_catch= Catches(team ="Sample1", species="Cod", weight = 1.23)
+        db.session.add(test_catch)
         db.session.commit()
 
     # Will be called after every test
@@ -47,4 +46,85 @@ class TestBase(TestCase):
         db.drop_all()
         
         
+class TestViews(TestBase):
+
+    def test_homepage(self):
+        response = self.client.get(url_for("homepage"))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_all_catches(self):
+        response = self.client.get(url_for("all_catches"))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_about(self):
+        response = self.client.get(url_for("about"))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_add_team(self):
+        response = self.client.get(url_for("add_team"))
+        data = dict(team="Sample1", email="sample1@sample.com", fee= True)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_add_catch(self):
+        response = self.client.get(url_for("add_catch"))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_update_team(self):
+        response = self.client.get(url_for("update", id=1))
+        self.assertEqual(response.status_code, 200)
     
+    def test_update_catch(self):
+        response = self.client.get(url_for("update_catch", id=1))
+        self.assertEqual(response.status_code, 200)        
+        
+               
+    def test_fee_paid(self):
+        response = self.client.get(url_for("fee_paid", id=1))
+        self.assertEqual(response.status_code, 302)
+
+    def test_fee_not_paid(self):
+        response = self.client.get(url_for("fee_not_paid", id=2))
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_team(self):
+        response = self.client.get(url_for("delete", id=2))
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_catch(self):
+        response = self.client.get(url_for("delete_catch", id=1))
+        self.assertEqual(response.status_code, 302)
+        
+        
+class TestCreate(TestBase):
+    def test_add_team(self):
+        response = self.client.post(
+            url_for('add_team'),
+            data = dict(team="Sample3", email="sample3@sample.com", fee= False)  
+            )
+        assert Teams.query.filter_by(team="Sample3").first().id ==3
+       
+    def test_add_catch(self):
+        response = self.client.post(
+            url_for('add_catch'),
+            data = dict(team ="Sample1", species="Mackerel", weight = 4.23)  
+            )
+        assert Catches.query.filter_by(species="Mackerel").first().id ==2
+       
+       
+class TestUpdate(TestBase):
+    
+    def test_update_team(self):
+        response = self.client.post(
+            url_for('update', id=2),
+              data = dict(team="SampleUpdated", email="sample3@sample.com", fee= False)  
+            )
+        assert Teams.query.filter_by(team="SampleUpdated").first().id ==2
+       
+       
+    def test_update_catch(self):
+        response = self.client.post(
+            url_for('update_catch',id=1),
+            data = dict(team ="Sample1", species="Cod", weight = 1.23)  
+            )
+        assert Catches.query.filter_by(team ="Sample1").first().id ==1
+       
