d7400e4920f4572768128ba0881ce7bb400cf302
rauliathos <rauliathos@gmail.comgit config --global user.email rauliathos@gmail.comgit config --global user.email rauliathos@gmail.com>
2022-07-02 02:29:41 +0000

    jenkins don't like secret key :(

--- application/__init__.py
+++ application/__init__.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+#app.config['SECRET_KEY'] = "shhhhhh"
 db = SQLAlchemy(app)
 
 from application import routes
